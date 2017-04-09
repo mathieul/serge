31034b99717a4f8e06cd023b3c967fecc06e7d64
Mathieu Lajugie <mathieu@caring.com>
2017-04-08 22:39:14 -0700

    WIP: still on it...

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -2,21 +2,28 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const elmSource = path.resolve(__dirname, './elm')
-
 module.exports = {
+  devtool: 'source-map',
   entry: ['bootstrap-loader', './elm/Tasker.elm', './js/app.js'],
   output: {
     path: path.resolve(__dirname, '../priv/static'),
     filename: 'js/app.js',
   },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, './node_modules'),
+      path.resolve(__dirname, './js'),
+    ],
+    extensions: ['.js', '.elm'],
+    alias: {
+      phoenix: path.resolve(__dirname, '../deps/phoenix/assets/js/phoenix.js'),
+      phoenix_html: path.resolve(__dirname, '../deps/phoenix_html/web/static/js/phoenix_html.js'),
+    }
+  },
   plugins: [
     new CopyWebpackPlugin([{ from: './static/', to: '../static/' }]),
     new ExtractTextPlugin({ filename: 'css/app.css', allChunks: true }),
   ],
-  resolve: {
-    extensions: ['.js', '.elm']
-  },
   module: {
     noParse: [/.elm$/],
     loaders: [
@@ -24,14 +31,27 @@ module.exports = {
         test:     /\.js$/,
         exclude:  [/node_modules/],
         loader:   'babel-loader',
-        query: { presets: ['env'] },
       },
       {
         test:    /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        loader:  'elm-hot!elm-webpack?verbose=true&warn=true&debug=true&cwd=' + elmSource,
+        exclude: [/elm-stuff/, /node_modules/,  /Stylesheets\.elm/],
+        loader:  'elm-hot-loader!elm-webpack-loader?verbose=true&warn=true&debug=true',
       },
-      { test: /bootstrap[\/\\]dist[\/\\]js[\/\\]umd[\/\\]/, loader: 'imports-loader' },
+      // {
+      //   test: /Stylesheets\.elm$/,
+      //   loader: 'style-loader!css-loader!elm-css-webpack-loader',
+      // },
+      {
+        test: /Stylesheets\.elm$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            'css-loader',
+            'elm-css-webpack-loader'
+          ]
+        })
+      },
+      { test: /bootstrap-loader\/dist\/js\/umd\//, loader: 'imports-loader' },
       { test: /\.(woff2?|svg)$/, loader: 'url-loader?limit=10000' },
       { test: /\.(ttf|eot)$/, loader: 'file-loader' },
     ],
