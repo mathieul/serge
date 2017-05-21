b884a5064bcce3ce07897d83e6ed99ce1f21aafc
Mathieu Lajugie <mathieu@caring.com>
2017-05-21 10:07:38 -0700

    fix dups in team dropdown

--- apps/serge_web/lib/serge_web/controllers/helpers.ex
+++ apps/serge_web/lib/serge_web/controllers/helpers.ex
@@ -9,13 +9,13 @@ defmodule Serge.Web.Controllers.Helpers do
   def shows_navigation_top_bar(conn, _options) do
     current_user = conn.assigns[:current_user]
     team_accesses = Serge.Scrumming.list_team_accesses(user: current_user)
-    names =
+    team_values =
       team_accesses
-      |> Enum.map(fn access -> access.team.name end)
+      |> Enum.map(fn access -> {access.team.id, access.team.name} end)
       |> Enum.dedup
 
     conn
     |> assign(:team_accesses, team_accesses)
-    |> assign(:team_names, names)
+    |> assign(:team_values, team_values)
   end
 end
